b9a0f57aed977b41ed04ddb44dca591a981ef0db
zakariaa <zakariaa.formisa@gmail.com>
2023-05-16 15:54:58 +0200

    add conf

--- src/App.js
+++ src/App.js
@@ -2,6 +2,16 @@ import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
+  var a = NaN;
+
+  if (a === NaN) {
+    // Noncompliant; always false
+    console.log("a is not a number"); // this is dead code
+  }
+  if (a !== NaN) {
+    // Noncompliant; always true
+    console.log("a is not NaN"); // this statement is not necessarily true
+  }
   return (
     <div className="App">
       {/* <header className="App-header">
